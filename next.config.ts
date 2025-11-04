e2353c6171077dccca8e733ebe8649bd7a360801
Samuel-Datubo-Jaja <jaja.samuel2019@gmail.com>
2025-11-04 08:54:13 +0000

    fix: use connection pooler for Vercel builds

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,6 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,  //deploy and coeme back to fix ts and eslit
   },
-  eslint: {
-    ignoreDuringBuilds: true,  
-  },
 };
 
 export default nextConfig;
